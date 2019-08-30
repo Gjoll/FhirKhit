3f3babd895675f251fe2b4bf22e4525c750e551c
Gjoll <kurta@gjoll.com>
2019-08-30 13:31:03 -0500

    WIP

--- Projects/SliceGen/R4/FhirKhit.SliceGen.XUnitTestsA.R4/GenCreateTests.cs
+++ Projects/SliceGen/R4/FhirKhit.SliceGen.XUnitTestsA.R4/GenCreateTests.cs
@@ -44,20 +44,6 @@ namespace FhirKhit.SliceGen.XUnitTests
               "US Core");
         }
 
-        String TestFile(String name) => $"{Path.Combine(GenCreateTests.TestDir(), name)}";
-
-
-        [Fact(DisplayName = "SliceGen.ConstructorTest")]
-        [Trait("Test", "test")]
-        public void ConstructorTest()
-        {
-            SliceGenerator p = new SliceGenerator(SliceGenerator.OutputLanguageType.CSharp,
-                OutputNameSpace,
-                GenDir);
-            Assert.True(p.OutputLanguage == SliceGenerator.OutputLanguageType.CSharp);
-            Assert.True(p.NameSpace == OutputNameSpace);
-            Assert.True(p.OutputDir == GenDir);
-        }
 
         StructureDefinition CreateObservation(String name)
         {
@@ -86,27 +72,6 @@ namespace FhirKhit.SliceGen.XUnitTests
                 });
 
             return profile;
-        }
-
-        void Create_NoModObservation(SliceGenerator p)
-        {
-            StructureDefinition profile = CreateObservation("NoModObservation");
-            SnapshotCreator.Create(profile);
-            p.AddProfile(profile);
-        }
-
-
-        void Create_CardinalityModObservation(SliceGenerator p)
-        {
-            StructureDefinition profile = CreateObservation("CardinalityModObservation");
-            {
-                ElementDefinition e = profile.Differential.Element.GetOrCreateElement("Observation.identifier");
-                e.Min = 0;
-                e.Max = "1";
-            };
-
-            SnapshotCreator.Create(profile);
-            p.AddProfile(profile);
         }
 
         void Create_SlicedMultiple(SliceGenerator p)
@@ -172,69 +137,6 @@ namespace FhirKhit.SliceGen.XUnitTests
             profile.SaveJson($@"c:\Temp\SlicedMultiple.json");
         }
 
-        void Create_Fixed(SliceGenerator p)
-        {
-            StructureDefinition profile = CreateObservation("Fixed");
-
-            {
-                ElementDefinition e = profile.Differential.Element.GetOrCreateElement("Observation.identifier");
-                e.Min = 0;
-                e.Max = "1";
-                e.Fixed = new Identifier("fixedIdentifierSystem", "fixedIdentifierValue");
-            }
-            {
-                ElementDefinition e = profile.Differential.Element.GetOrCreateElement("Observation.status");
-                e.Fixed = new Code("cancelled");
-                e.Min = 1;
-                e.Max = "1";
-            }
-            {
-                ElementDefinition e = profile.Differential.Element.GetOrCreateElement("Observation.code");
-                e.Min = 0;
-                e.Max = "1";
-                e.Fixed = new CodeableConcept("codeSystem", "codeCode", "codeDisplay", "codeText");
-            }
-
-            SnapshotCreator.Create(profile);
-            p.AddProfile(profile);
-            profile.SaveJson($@"c:\Temp\{profile.Name}.json");
-        }
-
-        void Create_Fixed1(SliceGenerator p)
-        {
-            StructureDefinition profile = CreateObservation("Fixed1");
-
-            {
-                ElementDefinition e = profile.Differential.Element.GetOrCreateElement("Observation.effective[x]");
-                e.Fixed = new FhirDateTime(1002, 1, 2, 3, 4, 5, new TimeSpan(0));
-            }
-
-            {
-                ElementDefinition e = profile.Differential.Element.GetOrCreateElement("Observation.value[x]");
-                e.Fixed = new Quantity(10, "things", "www.things.com");
-            }
-
-            SnapshotCreator.Create(profile);
-            p.AddProfile(profile);
-        }
-
-        void Create_Fixed2(SliceGenerator p)
-        {
-            StructureDefinition profile = CreateObservation("Fixed2");
-
-            {
-                ElementDefinition e = profile.Differential.Element.GetOrCreateElement("Observation.effective[x]");
-                e.Fixed = new Period(new FhirDateTime(1002, 1, 2), new FhirDateTime(1003, 4, 5));
-            }
-
-            {
-                ElementDefinition e = profile.Differential.Element.GetOrCreateElement("Observation.value[x]");
-                e.Fixed = new FhirBoolean(true);
-            }
-
-            SnapshotCreator.Create(profile);
-            p.AddProfile(profile);
-        }
 
         [Fact(DisplayName = "SliceGen.CreateGenTest")]
         [Trait("Test", "test")]
@@ -243,12 +145,6 @@ namespace FhirKhit.SliceGen.XUnitTests
             SliceGenerator p = new SliceGenerator(SliceGenerator.OutputLanguageType.CSharp,
                 OutputNameSpace,
                 GenDir);
-            //$this.Create_NoModObservation(p);
-            //$this.Create_CardinalityModObservation(p);
-            //$this.Create_Fixed(p);
-            //$this.Create_Fixed1(p);
-            //$this.Create_Fixed2(p);
-            //$this.Create_Components(p);
             this.Create_SlicedMultiple(p);
             bool success = p.Process();
 
@@ -257,85 +153,5 @@ namespace FhirKhit.SliceGen.XUnitTests
             Trace.WriteLine(sb.ToString());
             Assert.True(success == true);
         }
-
-        //[Fact(DisplayName = "SliceGen.StatusError")]
-        //[Trait("Test", "test")]
-        //void StatusError()
-        //{
-        //    ElementDefinition CreateElement(List<ElementDefinition> elements, String path, String id)
-        //    {
-        //        ElementDefinition retVal = new ElementDefinition
-        //        {
-        //            Path = path,
-        //            ElementId = id
-        //        };
-        //        elements.Add(retVal);
-
-        //        return retVal;
-        //    }
-
-        //    void OutputDef(String name, bool errorFlag)
-        //    {
-        //        StructureDefinition profile = new StructureDefinition
-        //        {
-        //            Url = $"http://xxyyz.com/{name}",
-        //            Name = name,
-        //            Title = name,
-        //            Description = new Markdown("Profile on Observation with fixed values"),
-        //            BaseDefinition = "http://hl7.org/fhir/StructureDefinition/Observation",
-        //            Derivation = StructureDefinition.TypeDerivationRule.Constraint,
-        //            DateElement = new FhirDateTime(2019, 1, 1, 0, 0, 0, new TimeSpan(0)),
-        //            Publisher = "Me",
-        //            FhirVersion = FVersion,
-        //            Type = "Observation",
-        //            Kind = StructureDefinition.StructureDefinitionKind.Resource,
-        //            Abstract = false,
-        //            Differential = new StructureDefinition.DifferentialComponent()
-        //        };
-
-        //        profile.Differential.Element.Add(
-        //            new ElementDefinition
-        //            {
-        //                Path = "Observation",
-        //                ElementId = "Observation"
-        //            });
-
-        //        if (errorFlag)
-        //        {
-        //            ElementDefinition e = CreateElement(profile.Differential.Element, "Observation.code", "Observation.code");
-        //            e.Min = 1;
-        //            e.Max = "1";
-        //            e.Fixed = new CodeableConcept("codeSystem", "codeCode", "codeDisplay", "codeText");
-        //        }
-
-        //        {
-        //            ElementDefinition e = CreateElement(profile.Differential.Element, "Observation.status", "Observation.status");
-        //            e.Fixed = new Code("cancelled");
-        //            e.Min = 1;
-        //            e.Max = "1";
-        //        }
-
-        //        // Generate StructureDefinition.Snapshot using c# API.
-        //        SnapshotGeneratorSettings settings = SnapshotGeneratorSettings.CreateDefault();
-        //        SnapshotGenerator generator = new SnapshotGenerator(ZipFhirSource.Source, settings);
-        //        generator.Update(profile);
-        //        String outputPath = $@"c:\Temp\{name}.json";
-        //        profile.SaveJson(outputPath);
-        //    }
-
-        //    OutputDef("statusError", true);
-        //    OutputDef("statusWorks", false);
-        //}
-
-        //[Fact(DisplayName = "SliceGen.CreateSnapshotOfBreastAbnormality")]
-        //[Trait("Test", "test")]
-        //public void CreateSnapshotOfBreastAbnormality()
-        //{
-        //    String path = Path.Combine(DirHelper.FindParentDir("FhirKhit"), "BreastAbnormality.json");
-        //    String jsonText = File.ReadAllText(path);
-        //    FhirJsonParser parser = new FhirJsonParser();
-        //    StructureDefinition profile = parser.Parse<StructureDefinition>(jsonText);
-        //    SnapshotCreator.Create(profile);
-        //}
     }
 }
