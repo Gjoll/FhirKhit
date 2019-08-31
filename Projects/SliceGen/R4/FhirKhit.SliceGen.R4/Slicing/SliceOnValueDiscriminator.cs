48af879e109215bb7044014f176d73b50d7ea319
Gjoll <kurta@gjoll.com>
2019-08-30 19:15:56 -0500

    WIP

---
+++ Projects/SliceGen/R4/FhirKhit.SliceGen.R4/Slicing/SliceOnValueDiscriminator.cs
@@ -0,0 +1,45 @@
+﻿using Hl7.Fhir.ElementModel;
+using Hl7.Fhir.FhirPath;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Model.Primitives;
+using Hl7.FhirPath;
+using Hl7.FhirPath.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FhirKhit.SliceGen.ShareLib
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SliceOnValueDiscriminator : ISliceDiscriminator 
+    {
+        public String Path {get; set; }
+        public Base Pattern { get; set; }
+
+        /// <summary>
+        /// Return <see langword="true"/>if item is in slice.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool IsSlice(Element discriminator,
+            Element item)
+        {
+            var nav = item.ToTypedElement();
+            EvaluationContext ctx = new FhirEvaluationContext();
+            ITypedElement[] results = nav.Select(Path, ctx).ToArray();
+            if ((results == null) || (results.Length == 0))
+                return false;
+            if (results.Length > 1)
+                throw new NotImplementedException($"SliceOnValueDiscriminator.GetValue: Multiple elements found at path '{this.Path}'");
+            ITypedElement result = results[0];
+            Element value = result.Value as Element;
+            if (value == null)
+                throw new NotImplementedException($"SliceOnValueDiscriminator.GetValue: Value is not an Element '{this.Path}'");
+            //# TODO: I am using Matches here. SHould I be using IsExactly?
+            return value.Matches(discriminator);
+        }
+    }
+}
---
+++ Projects/SliceGen/R4/FhirKhit.SliceGen.R4/Slicing/Slicer.cs
@@ -0,0 +1,18 @@
+﻿using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FhirKhit.SliceGen.ShareLib
+{
+    public class Slicer<TBase>
+        where TBase : Base
+    {
+        Slicing sliceDiscriminator;
+
+        public Slicer(Slicing sliceDiscriminator)
+        {
+            this.sliceDiscriminator = sliceDiscriminator;
+        }
+    }
+}
