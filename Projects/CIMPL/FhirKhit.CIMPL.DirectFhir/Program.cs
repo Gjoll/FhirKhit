f4c41061e11ec1ef987a0d8e6e545a2373904610
Gjoll <KurtA@Gjoll.com>
2019-09-17 21:40:14 -0400

    .

---
+++ Projects/CIMPL/FhirKhit.CIMPL.DirectFhir/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FhirKhit.CIMPL.DirectFhir
+{
+    class Program
+    {
+        static string inputFile;
+        static string outputDir;
+
+        static String GetArgs(string[] args, ref Int32 i, String errorMsg)
+        {
+            if (i >= args.Length)
+                    throw new Exception(errorMsg);
+            return args[i++];
+        }
+
+        static void ParseArgs(string[] args)
+        {
+            Int32 i = 0;
+            while (i < args.Length)
+            {
+                String arg = args[i++];
+                switch (arg.Trim().ToLower())
+                {
+                    case "-i":
+                        inputFile = GetArgs(args, ref i, "Missing argument to -i parameter");
+                        break;
+                    case "-o":
+                        outputDir = GetArgs(args, ref i, "Missing argument to -o parameter");
+                        break;
+                    default:
+                        throw new Exception("Unknown command line argument {arg}");
+                }
+            }
+
+            if (String.IsNullOrEmpty(inputFile))
+                throw new Exception($"Missing required -i argument");
+
+            if (String.IsNullOrEmpty(outputDir))
+                throw new Exception($"Missing required -o argument");
+        }
+        static Int32 Main(string[] args)
+        {
+            try
+            {
+                ParseArgs(args);
+                DirectFhirGenerator dfg = new DirectFhirGenerator();
+                return dfg.Execute(inputFile, outputDir);
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return -1;
+            }
+        }
+    }
+}
---
+++ Projects/Tools/FhirKhit.Tools/Conversion/ConvertErrorException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FhirKhit.Tools
+{
+    public class ConvertErrorException : Exception
+    {
+        public String FileName;
+        public String MethodName;
+
+        public ConvertErrorException(String fileName, String methodName, String message) : base(message)
+        {
+            this.FileName = fileName;
+            this.MethodName = methodName;
+        }
+    }
+}
