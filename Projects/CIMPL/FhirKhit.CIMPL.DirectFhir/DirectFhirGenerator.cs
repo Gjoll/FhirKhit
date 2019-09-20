b79280607fd13e1ccb1233fcc1cb278b25b5431a
Gjoll <KurtA@Gjoll.com>
2019-09-20 13:11:48 -0400

    Added load stage, Group vs Entry checking.

--- Projects/CIMPL/FhirKhit.CIMPL.DirectFhir/DirectFhirGenerator.cs
+++ Projects/CIMPL/FhirKhit.CIMPL.DirectFhir/DirectFhirGenerator.cs
@@ -14,16 +14,22 @@ namespace FhirKhit.CIMPL.DirectFhir
 {
     public class DirectFhirGenerator : ConverterBase, IDisposable
     {
-        public ZipSource Source
+        class SDefInfo
         {
-            get
+            public enum TypeFlag
             {
-                if (source == null)
-                    source = new ZipSource("specification.zip");
-                return source;
+                Unknown,
+                Group,
+                Entry
             }
-        }
-        ZipSource source = null;
+            public TypeFlag TFlag = TypeFlag.Unknown;
+
+            public StructureDefinition SDef;
+        };
+
+        Dictionary<String, SDefInfo> items;
+
+        ZipSource source;
 
         CodeEditor entryEditor = new CodeEditor();
         CodeBlockNested entryBlock;
@@ -31,11 +37,17 @@ namespace FhirKhit.CIMPL.DirectFhir
         CodeEditor mapEditor = new CodeEditor();
         CodeBlockNested mapBlock;
 
-        HashSet<String> baseDefinitions = new HashSet<string>();
-
-
-        void ProcessSchemaItemSpecialiation(StructureDefinition sDef)
+        public DirectFhirGenerator()
         {
+            source = new ZipSource("specification.zip");
+            this.items = new Dictionary<string, SDefInfo>();
+        }
+
+
+        void ProcessSchemaItemSpecialiation(SDefInfo sDefInfo)
+        {
+            StructureDefinition sDef = sDefInfo.SDef;
+
             const string fcn = "ProcessSchemaItemSpecialiation";
 
             CodeBlockNested item = entryBlock.AppendBlock();
@@ -63,16 +75,35 @@ namespace FhirKhit.CIMPL.DirectFhir
                 case "unsignedInt":
                 case "positiveInt":
                 case "xhtml":
-                    this.ConversionInfo(this.GetType().Name, fcn, $"Ignoring '{name}' because it dewrives from primitive '{parent}'");
+                    this.ConversionInfo(this.GetType().Name, fcn, $"Ignoring '{name}' because it derives from primitive '{parent}'");
                     return;
+            }
+            String typeName;
+            switch (sDefInfo.TFlag)
+            {
+                case SDefInfo.TypeFlag.Entry:
+                    typeName = "Entry";
+                    break;
+                case SDefInfo.TypeFlag.Group:
+                    typeName = "Group";
+                    break;
+                default:
+                    throw new ConvertErrorException(this.GetType().Name, fcn, $"Invalid TFlag value");
             }
 
             item
                 .BlankLine()
                 .Comment(description)
-                .AppendCode($"Entry: {name}")
+                .AppendCode($"{typeName}: {name}")
                 .AppendCode($"Parent: {parent}")
                 ;
+
+            foreach (ElementDefinition ed in sDef.Differential.Element.Skip(1))
+            {
+                if (ed.Path.Split('.').Length != 2)
+                    throw new ConvertErrorException(this.GetType().Name, fcn, $"Invalid element {ed.Path}. Too many parts. Expected 2");
+                String propertyName = ed.Path.LastPathPart();
+            }
 
             //    foreach (JToken property in properties.Children())
             //    {
@@ -84,11 +115,11 @@ namespace FhirKhit.CIMPL.DirectFhir
             //    }
         }
 
-        void ProcessFhirElement(StructureDefinition sDef)
+        void ProcessFhirElement(SDefInfo sDefInfo)
         {
+            StructureDefinition sDef = sDefInfo.SDef;
+
             String baseDefinition = sDef.BaseDefinition;
-            if (this.baseDefinitions.Contains(baseDefinition) == false)
-                this.baseDefinitions.Add(baseDefinition);
             switch (sDef.BaseDefinition)
             {
                 case "http://hl7.org/fhir/StructureDefinition/Element":
@@ -97,33 +128,69 @@ namespace FhirKhit.CIMPL.DirectFhir
 
                 default:
                     if (sDef.Derivation == StructureDefinition.TypeDerivationRule.Specialization)
-                        ProcessSchemaItemSpecialiation(sDef);
+                        ProcessSchemaItemSpecialiation(sDefInfo);
                     break;
             }
         }
 
-
-        void ProcessFhirElement(String uri)
+        void LoadFhirElements()
         {
-            //const String fcn = "ProcessFhirElement";
+            // const String fcn = "ProcessFhirElements";
 
-            switch (Source.ResolveByUri(uri))
+            foreach (string uri in this.source.ListResourceUris())
             {
-                case StructureDefinition sDef:
-                    ProcessFhirElement(sDef);
-                    break;
+                StructureDefinition sDef = source.ResolveByUri(uri) as StructureDefinition;
+                if (sDef != null)
+                {
+                    SDefInfo sDefInfo = new SDefInfo
+                    {
+                        SDef = sDef,
+                        TFlag = SDefInfo.TypeFlag.Unknown
+                    };
+
+                    switch (uri)
+                    {
+                        case "http://hl7.org/fhir/StructureDefinition/Element":
+                            sDefInfo.TFlag = SDefInfo.TypeFlag.Group;
+                            break;
+
+                        case "http://hl7.org/fhir/StructureDefinition/Resource":
+                            sDefInfo.TFlag = SDefInfo.TypeFlag.Entry;
+                            break;
+                    }
+
+                    this.items.Add(sDef.Url, sDefInfo);
+                }
             }
+        }
+
+        SDefInfo GetTypedSDef(String path)
+        {
+            const String fcn = "GetTypedSDef";
+
+            if (this.items.TryGetValue(path, out SDefInfo sDef) == false)
+                throw new ConvertErrorException(this.GetType().Name, fcn, $"Internal error. Item {path} not in dictionary");
+
+            // If TFlag s unknown, use parents TFlag value.
+            if (sDef.TFlag == SDefInfo.TypeFlag.Unknown)
+            {
+                String baseDefinition = sDef.SDef.BaseDefinition;
+                SDefInfo parentInfo = this.GetTypedSDef(baseDefinition);
+                sDef.TFlag = parentInfo.TFlag;
+            }
+
+            return sDef;
         }
 
         void ProcessFhirElements()
         {
-            // const String fcn = "ProcessFhirElements";
+            const String fcn = "ProcessFhirElements";
 
-            foreach (string uri in this.Source.ListResourceUris())
-                ProcessFhirElement(uri);
-
-            foreach (String baseDef in this.baseDefinitions)
-                Trace.WriteLine(baseDef);
+            foreach (string path in this.items.Keys)
+            {
+                SDefInfo sDef = GetTypedSDef(path);
+                ProcessFhirElement(sDef);
+            }
         }
 
         void CreateEditors(String outputDir)
@@ -161,6 +228,7 @@ namespace FhirKhit.CIMPL.DirectFhir
             try
             {
                 CreateEditors(outputDir);
+                LoadFhirElements();
                 ProcessFhirElements();
                 SaveEditors();
             }
