2419981738839052832a52757fa6abeb835b2a4d
Gjoll\Kurt <KurtA@Gjoll.com>
2019-09-24 20:32:18 -0500

    WIP

--- Projects/CIMPL/FhirKhit.CIMPL.DirectFhir/ConvertFhirClass.cs
+++ Projects/CIMPL/FhirKhit.CIMPL.DirectFhir/ConvertFhirClass.cs
@@ -16,11 +16,18 @@ namespace FhirKhit.CIMPL.DirectFhir
     /// </summary>
     class ConvertFhirClass
     {
+        const String BackboneElementStr = "BackboneElement";
+        const String ElementStr = "Element";
+        const String ResourceStr = "Resource";
+
         DirectFhirGenerator gen;
         DirectFhirGenerator.SDefInfo sDefInfo;
         StructureDefinition sDef;
         Int32 elementIndex;
         ElementDefinition[] elements;
+        CodeEditor entryEditor;
+        CodeEditor mapEditor;
+        Dictionary<String, ElementDefinition> propertyNames = new Dictionary<String, ElementDefinition>();
 
         public ConvertFhirClass(DirectFhirGenerator gen,
             DirectFhirGenerator.SDefInfo sDefInfo)
@@ -72,6 +79,31 @@ namespace FhirKhit.CIMPL.DirectFhir
             this.ProcessEntry(this.sDef.Id, null, typeName, parent, description, this.sDef.Url);
         }
 
+        String UniquePropertyName(String baseName, String pName, Int32 counter, ElementDefinition ed, out bool createFlag)
+        {
+            if (this.propertyNames.TryGetValue(pName, out ElementDefinition edOld) == false)
+            {
+                createFlag = true;
+                this.propertyNames.Add(pName, ed);
+                return pName;
+            }
+
+            // if old element and this one have same type, then use same definition.
+            if ((edOld != null) && (ed.Type.IsExactly(edOld.Type)))
+            {
+                createFlag = false;
+                return pName;
+            }
+
+            counter += 1;
+            return UniquePropertyName(baseName, $"{baseName}{counter}", counter, ed, out createFlag);
+        }
+
+        String UniquePropertyName(ElementDefinition ed, out bool createFlag)
+        {
+            String pName = ed.Path.LastPathPart().ToMachineName();
+            return UniquePropertyName(pName, pName, 1, ed, out createFlag);
+        }
 
         /// <summary>
         /// Create new file containing the definition for the new item.
@@ -86,16 +118,19 @@ namespace FhirKhit.CIMPL.DirectFhir
         {
             const string fcn = "ProcessEntry";
 
+            this.propertyNames.Add("Value", null);
             switch (path)
             {
                 // Hand code "Element" type. Element appears to have a non standard typeref
                 // which the c# library doesn't handle (_code = extension....)
-                case "Element":
+                case ElementStr:
                     {
+                        this.entryEditor = this.gen.CreateEntryEditor(path);
+                        this.mapEditor = this.gen.CreateMapEditor(path);
                         ElementDefinition sole = this.elements[0];
-                        this.elements = new ElementDefinition[] {sole};
-                        CodeBlockNested mapBlock = this.gen.CreateMapEditor(path);
-                        this.ProcessSubEntry(0, mapBlock, path, path, suffix, typeName, parent, description, comment);
+                        this.elements = new ElementDefinition[] { sole };
+                        this.propertyNames.Add(ElementStr, null);
+                        this.ProcessSubEntry(0, path, path, suffix, typeName, parent, description, comment);
                         if (this.elementIndex != this.elements.Length)
                             throw new ConvertErrorException(this.GetType().Name, fcn, $"Internal error. ElementIndex not correct");
                     }
@@ -127,8 +162,13 @@ namespace FhirKhit.CIMPL.DirectFhir
 
                 default:
                     {
-                        CodeBlockNested mapBlock = this.gen.CreateMapEditor(path);
-                        this.ProcessSubEntry(0, mapBlock, path, path, suffix, typeName, parent, description, comment);
+                        this.entryEditor = this.gen.CreateEntryEditor(path);
+                        this.mapEditor = this.gen.CreateMapEditor(path);
+
+                        CodeBlockNested mapBlock = mapEditor.Blocks.AppendBlock();
+                        String entryName = path.LastPathPart().ToMachineName();
+                        this.propertyNames.Add(entryName, null);
+                        this.ProcessSubEntry(0, path, path, suffix, typeName, parent, description, comment);
                         if (this.elementIndex != this.elements.Length)
                             throw new ConvertErrorException(this.GetType().Name, fcn, $"Internal error. ElementIndex not correct");
                     }
@@ -141,7 +181,6 @@ namespace FhirKhit.CIMPL.DirectFhir
         /// The new entry is created in its own namespace.
         /// </summary>
         String ProcessSubEntry(Int32 indent,
-            CodeBlockNested mapBlock,
             String elementPath,
             String entryPath,
             String suffix,
@@ -150,9 +189,9 @@ namespace FhirKhit.CIMPL.DirectFhir
             String description,
             String comment)
         {
-            CodeBlockNested entryBlock = this.gen.CreateEntryEditor(elementPath);
-            CodeBlockNested classBlock = entryBlock.AppendBlock();
-            CodeBlockNested propertydefinitionsBlock = entryBlock.AppendBlock();
+            CodeBlockNested classBlock = entryEditor.Blocks.AppendBlock();
+            CodeBlockNested propertydefinitionsBlock = entryEditor.Blocks.AppendBlock();
+            CodeBlockNested mapBlock = mapEditor.Blocks.AppendBlock();
 
             String entryName = elementPath.LastPathPart().ToMachineName();
             if (String.IsNullOrEmpty(suffix) == false)
@@ -184,8 +223,9 @@ namespace FhirKhit.CIMPL.DirectFhir
             switch (parent)
             {
                 case null:
-                case "Resource":
-                case "Element":
+                case ResourceStr:
+                case BackboneElementStr:
+                case ElementStr:
                     break;
 
                 default:
@@ -202,10 +242,10 @@ namespace FhirKhit.CIMPL.DirectFhir
                 ElementDefinition subElement = this.elements[this.elementIndex];
                 if (subElement.Path.StartsWith($"{elementPath}.") == false)
                     break;
-                this.ProcessProperty(indent, classBlock, propertydefinitionsBlock, mapBlock, elementPath, entryPath, entryName);
+                this.ProcessProperty(indent, classBlock, propertydefinitionsBlock, mapBlock, elementPath, entryPath);
             }
 
-            return $"{this.gen.NameSpace(elementPath)}.{entryName}";
+            return entryName;
         }
 
 
@@ -235,12 +275,9 @@ namespace FhirKhit.CIMPL.DirectFhir
             CodeBlockNested propertiesBlock,
             CodeBlockNested mapBlock,
             String elementPath,
-            String entryPath,
-            String entryName)
+            String entryPath)
         {
             const string fcn = "ProcessProperty";
-            const String BackboneElementStr = "BackboneElement";
-            const String ElementStr = "Element";
 
             ElementDefinition ed = this.elements[this.elementIndex++];
 
@@ -264,11 +301,7 @@ namespace FhirKhit.CIMPL.DirectFhir
             //if (ed.Pattern != null)
             //    throw new ConvertErrorException(this.GetType().Name, fcn, $"Unexpected Pattern in element {ed.Path}.");
 
-            String propertyName = ed.Path.LastPathPart().ToMachineName();
-            if (propertyName == "Value")
-                propertyName = "ValueZ";
-            if (propertyName == entryName)
-                propertyName = $"{propertyName}Value";
+            String propertyName = UniquePropertyName(ed, out bool createFlag);
 
             String fullPropertyName;
             String propertyPath = $"{entryPath}.{propertyName}";
@@ -280,7 +313,7 @@ namespace FhirKhit.CIMPL.DirectFhir
                 if (this.HasChildren(ed) == false)
                     throw new ConvertErrorException(this.GetType().Name, fcn, $"Backbone element {ed.Path} has no children.");
 
-                fullPropertyName = this.ProcessSubEntry(indent + 1, mapBlock, ed.Path, propertyPath, "Group", "Group", BackboneElementStr, $"Group definition of {ed.Path}", null);
+                fullPropertyName = this.ProcessSubEntry(indent + 1, ed.Path, propertyPath, "Group", "Group", BackboneElementStr, $"Group definition of {ed.Path}", null);
             }
             else if (this.ContainsType(ed, ElementStr))
             {
@@ -289,108 +322,108 @@ namespace FhirKhit.CIMPL.DirectFhir
                 if (this.HasChildren(ed) == false)
                     throw new ConvertErrorException(this.GetType().Name, fcn, $"Element {ed.Path} has no children.");
 
-                fullPropertyName = this.ProcessSubEntry(indent + 1, mapBlock, ed.Path, propertyPath, "Group", "Group", ElementStr, $"Group definition of {ed.Path}", null);
+                fullPropertyName = this.ProcessSubEntry(indent + 1, ed.Path, propertyPath, "Group", "Group", ElementStr, $"Group definition of {ed.Path}", null);
             }
             else
             {
-                propertiesBlock
-                    .BlankLine()
-                    .AppendLine($"// Entry definition of {ed.Path}")
-                    .AppendCode($"Element: {propertyName}")
-                    ;
-
-                HashSet<String> outputTypes = new HashSet<string>();
-                bool firstFlag = true;
-                void OutputType(String pType)
+                fullPropertyName = propertyName;
+                if (createFlag)
                 {
-                    if (outputTypes.Contains(pType))
-                        return;
-                    outputTypes.Add(pType);
-                    if (firstFlag)
-                        propertiesBlock.AppendCode($"Value: {pType}");
-                    else
-                        propertiesBlock.AppendCode($"    or {pType}");
-                    firstFlag = false;
-                }
-
-                mapBlock.AppendCode($"    {propertyPath.SkipFirstPathPart()} maps to {ed.Path.SkipFirstPathPart()}");
-                fullPropertyName = $"{this.gen.NameSpace(elementPath)}.{propertyName}";
-                foreach (ElementDefinition.TypeRefComponent type in ed.Type)
-                {
-                    switch (type.Code)
+                    propertiesBlock
+                        .BlankLine()
+                        .AppendLine($"// Entry definition of {ed.Path}")
+                        .AppendCode($"Element: {propertyName}")
+                        ;
+                    HashSet<String> outputTypes = new HashSet<string>();
+                    bool firstFlag = true;
+                    void OutputType(String pType)
                     {
-                        case null:
-                            break;
+                        if (outputTypes.Contains(pType))
+                            return;
+                        outputTypes.Add(pType);
+                        if (firstFlag)
+                            propertiesBlock.AppendCode($"Value: {pType}");
+                        else
+                            propertiesBlock.AppendCode($"    or {pType}");
+                        firstFlag = false;
+                    }
 
-                        case "boolean":
-                        case "integer":
-                        case "decimal":
-                        case "uri":
-                        case "string":
-                        case "base64Binary":
-                        case "instant":
-                        case "date":
-                        case "dateTime":
-                        case "time":
-                        case "oid":
-                        case "id":
-                        case "markdown":
-                        case "unsignedInt":
-                        case "positiveInt":
-                        case "xhtml":
-                            OutputType($"{type.Code}");
-                            break;
+                    mapBlock.AppendCode($"    {propertyPath.SkipFirstPathPart()} maps to {ed.Path.SkipFirstPathPart()}");
+                    foreach (ElementDefinition.TypeRefComponent type in ed.Type)
+                    {
+                        switch (type.Code)
+                        {
+                            case null:
+                                break;
 
-                        case "CodeableConcept":
-                        case "Coding":
-                        case "code":
-                            OutputType($"concept");
-                            break;
+                            case "boolean":
+                            case "integer":
+                            case "decimal":
+                            case "uri":
+                            case "string":
+                            case "base64Binary":
+                            case "instant":
+                            case "date":
+                            case "dateTime":
+                            case "time":
+                            case "oid":
+                            case "id":
+                            case "markdown":
+                            case "unsignedInt":
+                            case "positiveInt":
+                            case "xhtml":
+                                OutputType($"{type.Code}");
+                                break;
 
-                        case "uuid":
-                        case "url":
-                        case "canonical":
-                            OutputType($"uri");
-                            break;
+                            case "CodeableConcept":
+                            case "Coding":
+                            case "code":
+                                OutputType($"concept");
+                                break;
 
-                        case "Reference":
-                            if (type.Profile.Any())
-                                throw new ConvertErrorException(this.GetType().Name, fcn, $"Unexpected profile in type {ed.Path}:{type.Code}.");
-                            if (type.TargetProfile.Count() == 0)
-                            {
-                                this.gen.AddResourceToProcess("Resource", true);
-                                OutputType($"{this.gen.NameSpace("Resource")}.Resource");
-                            }
-                            else
-                            {
-                                foreach (string target in type.TargetProfile)
+                            case "uuid":
+                            case "url":
+                            case "canonical":
+                                OutputType($"uri");
+                                break;
+
+                            case "Reference":
+                                if (type.Profile.Any())
+                                    throw new ConvertErrorException(this.GetType().Name, fcn, $"Unexpected profile in type {ed.Path}:{type.Code}.");
+                                if (type.TargetProfile.Count() == 0)
                                 {
-                                    this.gen.AddResourcePathToProcess(target, true);
-                                    String targetEntryName = target.LastUriPart().ToMachineName();
-                                    OutputType($"{this.gen.NameSpace(targetEntryName)}.{targetEntryName}");
+                                    this.gen.AddResourceToProcess(ResourceStr, true);
+                                    OutputType($"{this.gen.NameSpace(ResourceStr)}.Resource");
                                 }
-                            }
-                            break;
-
-                        default:
-
-                            if (type.Profile.Count() == 0)
-                            {
-                                this.gen.AddResourceToProcess(type.Code, true);
-                                OutputType($"{this.gen.NameSpace(type.Code)}.{type.Code.ToMachineName()}");
-                            }
-                            else
-                            {
-                                foreach (string profile in type.Profile)
+                                else
                                 {
-                                    this.gen.AddResourcePathToProcess(profile, true);
-                                    String profileName = profile.LastUriPart().ToMachineName();
-                                    OutputType($"{this.gen.NameSpace(profileName)}.{profileName}");
+                                    foreach (string target in type.TargetProfile)
+                                    {
+                                        this.gen.AddResourcePathToProcess(target, true);
+                                        String targetEntryName = target.LastUriPart().ToMachineName();
+                                        OutputType($"{this.gen.NameSpace(targetEntryName)}.{targetEntryName}");
+                                    }
                                 }
-                            }
+                                break;
 
+                            default:
 
-                            break;
+                                if (type.Profile.Count() == 0)
+                                {
+                                    this.gen.AddResourceToProcess(type.Code, true);
+                                    OutputType($"{this.gen.NameSpace(type.Code)}.{type.Code.ToMachineName()}");
+                                }
+                                else
+                                {
+                                    foreach (string profile in type.Profile)
+                                    {
+                                        this.gen.AddResourcePathToProcess(profile, true);
+                                        String profileName = profile.LastUriPart().ToMachineName();
+                                        OutputType($"{this.gen.NameSpace(profileName)}.{profileName}");
+                                    }
+                                }
+                                break;
+                        }
                     }
                 }
             }
--- Projects/Tools/FhirKhit.Tools/Conversion/ConvertErrorException.cs
+++ Projects/Tools/FhirKhit.Tools/Conversion/ConvertErrorException.cs
@@ -6,13 +6,25 @@ namespace FhirKhit.Tools
 {
     public class ConvertErrorException : Exception
     {
-        public String FileName;
-        public String MethodName;
+        public String FileName { get; }
+        public String MethodName { get; }
 
         public ConvertErrorException(String fileName, String methodName, String message) : base(message)
         {
             this.FileName = fileName;
             this.MethodName = methodName;
+        }
+
+        public ConvertErrorException()
+        {
+        }
+
+        public ConvertErrorException(string message) : base(message)
+        {
+        }
+
+        public ConvertErrorException(string message, Exception innerException) : base(message, innerException)
+        {
         }
     }
 }
