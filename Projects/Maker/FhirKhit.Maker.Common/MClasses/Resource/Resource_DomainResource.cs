d381a246e08eb9e0ad0cae506746afe9caffae75
Gjoll\Kurt <KurtA@Gjoll.com>
2019-10-02 23:05:18 -0500

    .

--- Projects/Maker/FhirKhit.Maker.Common/MClasses/Resource/Resource_DomainResource.cs
+++ Projects/Maker/FhirKhit.Maker.Common/MClasses/Resource/Resource_DomainResource.cs
@@ -7,6 +7,110 @@ namespace FhirKhit.Maker.Common.Resource
 {
     public partial class Resource_DomainResource
     {
+        /*
+        <StructureDefinition xmlns="http://hl7.org/fhir">
+          <url value="http://example.org/fhir/StructureDefinition/participation-agreement"/>
+          <name value="Example Extension Definition"/>
+          <!-- snip other metadata -->
+          <kind value="complex-type"/>
+          <context>
+            <type value="element"/>
+            <expression value="Patient"/>
+          </context>
+          <type value="Extension"/>
+          <baseDefinition value="http://hl7.org/fhir/StructureDefinition/Extension"/>
+          <derivation value="constraint"/>
+
+          <differential>
+            <element>
+              <path value="Extension"/>
+              <short value="Agreed agreement/policy"/>
+              <definition value="A URI that identifies a participation agreement/policy
+                to which the patient has agreed"/>
+              <comment value="URI is a literal reference to agreement text (html).
+                Systems SHALL conform to the policies as indicated.
+                For further information, see the partnership agreement..."/>
+              <mustSupport value="true"/>
+              <isModifier value="false"/>
+            </element>
+            <element>
+              <path value="Extension.url"/>
+              <fixedUri value="http://example.org/fhir/StructureDefinition/participation-agreement"/>
+            </element>
+            <element>
+              <path value="Extension.valueUri"/>
+              <short value="The URI value"/>
+              <min value="1"/>
+              <max value="*"/>
+              <type>
+                 <code value="uri"/>
+              </type>
+              <mustSupport value="true"/>
+            </element>
+          </differential>
+        </StructureDefinition>
+        */
+        /*
+        <StructureDefinition xmlns="http://hl7.org/fhir">
+          <id value="patient-profile"/>
+          <!-- snip other metadata, narrative -->
+          <differential>
+
+            <!-- first, the patient root element
+              - can be copy/paste from the base patient profile -->
+            <element>
+              <path value="Patient"/>
+              <!-- snip definition -->
+            </element>
+
+            <!-- now, the general definition for extensions
+              - can be copied/pasted from the base patient profile,
+              with changes for slicing  -->
+            <element>
+              <path value="Patient.extension"/>
+              <!-- we're going to slice the extension element, and
+                one of the extensions is one we have defined -->
+              <slicing>
+                <!-- extension is always sliced on url -->
+                <discriminator>
+                  <type value="value"/>
+                  <path value="url"/>
+                </discriminator>
+                <!-- we don't care what the order of any extensions is -->
+                <ordered value="false"/>
+                <!-- Other extensions are allowed in addition to this one -->
+                <rules value="open"/>
+              </slicing>
+              <!-- snip definition -->
+            </element>
+
+            <!-- now, the slice that contains our extension -->
+            <element>
+              <path value="Patient.extension"/>
+              <sliceName value="agreement"/>
+              <!-- clone information from the extension definition.
+                duplicative, but this duplication makes it simpler overall -->
+              <short value="Agreed agreement/policy"/>
+              <definition value="A URI that identifies a participation agreement/policy
+              to which the patient has agreed"/>
+              <!--  min has to be 1, since the extension itself has min = 1 -->
+              <min value="1"/>
+              <max value="*"/>
+              <type>
+                <!-- obviously it has to be an extension -->
+                <code value="Extension"/>
+                <!-- and here is the link to the extension definition:
+                  this extension has to conform to the rules laid down in its definition -->
+                <profile value="http://example.org/phr/documents/fhir/StructureDefinition/participation-agreement"/>
+              </type>
+              <isModifier value="false"/>
+            </element>
+
+          <!-- snip the rest of the profile -->
+
+          </differential>
+        </StructureDefinition>
+        */
         public void AddExtensionSlice()
         {
             ElementDefinition extDef = this.Element_Extension.CreateConstraint();
