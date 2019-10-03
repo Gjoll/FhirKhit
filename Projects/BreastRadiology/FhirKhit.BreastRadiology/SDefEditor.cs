e23c66a2ae0a190a66eb44840fd4a2aa6c45fe14
Gjoll\Kurt <KurtA@Gjoll.com>
2019-10-03 15:17:37 -0500

    .

--- Projects/BreastRadiology/FhirKhit.BreastRadiology/SDefEditor.cs
+++ Projects/BreastRadiology/FhirKhit.BreastRadiology/SDefEditor.cs
@@ -2,17 +2,19 @@
 using Hl7.Fhir.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
 
 namespace FhirKhit.BreastRadiology
 {
     class SDefEditor
     {
-        Int32 selectionIndex = 0;
-
-        StructureDefinition baseSDef;
-        StructureDefinition sDef;
-        String basePath;
+        class ElementIndex
+        {
+            public Int32 Index;
+            public ElementDefinition ElementDefinition;
+        }
 
         static FhirStructureDefinitions Defs
         {
@@ -28,10 +30,28 @@ namespace FhirKhit.BreastRadiology
         }
         static FhirStructureDefinitions defs;
 
+        StructureDefinition baseSDef;
+        StructureDefinition sDef;
+        String basePath;
+
+        Dictionary<String, ElementIndex> baseElements = new Dictionary<string, ElementIndex>();
+        Dictionary<String, ElementIndex> elements = new Dictionary<string, ElementIndex>();
+
         public SDefEditor(String baseDefinition)
         {
             baseSDef = Defs.GetResource(baseDefinition);
             basePath = $"{this.baseSDef.Snapshot.Element[0].Path}.";
+
+            for (Int32 i = 0; i < baseSDef.Snapshot.Element.Count; i++)
+            {
+                ElementDefinition elementDefinition = baseSDef.Snapshot.Element[i];
+                ElementIndex e = new ElementIndex
+                {
+                    Index = i,
+                    ElementDefinition = elementDefinition
+                };
+                this.baseElements.Add(elementDefinition.Path, e);
+            }
 
             sDef = new StructureDefinition
             {
@@ -42,41 +62,56 @@ namespace FhirKhit.BreastRadiology
             sDef.Differential.Element.Add(baseSDef.Differential.Element[0]);
         }
 
-        public ElementDefinition Find(String path)
-        {
-            if (path.StartsWith(this.basePath) == false)
-                path = $"{basePath}{path}";
-
-            while (this.selectionIndex < this.baseSDef.Snapshot.Element.Count)
-            {
-                ElementDefinition e = this.baseSDef.Snapshot.Element[this.selectionIndex];
-                if (e.Path == path)
-                    return e;
-                this.selectionIndex += 1;
-            }
-            throw new Exception($"'{path}' not found");
-        }
-
         /// <summary>
         /// Select a element definition from the base sdef by its path name and 
         /// copy it to the differential of the output sdef.
-        /// Selections must be called in order, meaning that is item # has been selected,
-        /// attempts to select item #y (y < x) will fail.
-        /// This is because the element definitions in the output diofferential must be ordered
-        /// as they are in the base definition. Items can be missing, but can not be reordered.
         /// </summary>
         /// <param name="path"></param>
         /// <returns></returns>
         public ElementDefinition Select(String path)
         {
-            ElementDefinition e = this.Find(path);
-            this.sDef.Differential.Element.Add(e);
-            return e;
+            if (path.StartsWith(this.basePath) == false)
+                path = $"{basePath}{path}";
+
+            {
+                if (this.elements.TryGetValue(path, out ElementIndex e) == true)
+                    return e.ElementDefinition;
+            }
+            {
+                if (this.baseElements.TryGetValue(path, out ElementIndex e) == true)
+                {
+                    ElementDefinition ed = new ElementDefinition
+                    {
+                        Path = e.ElementDefinition.Path,
+                        ElementId = e.ElementDefinition.ElementId
+                    };
+
+                    ElementIndex newE = new ElementIndex
+                    {
+                        Index = e.Index,
+                        ElementDefinition = ed
+                    };
+                    this.elements.Add(ed.Path, newE);
+                    return ed;
+                }
+            }
+            throw new Exception($"'{path}' not found");
         }
 
         public void Write(String outputDir)
         {
+            List<ElementIndex> eList = this.elements.Values.ToList();
+            eList.Sort((a, b) =>
+            {
+                return a.Index.CompareTo(b.Index);
+            });
+            foreach (ElementIndex item in eList)
+            {
+                this.sDef.Differential.Element.Add(item.ElementDefinition);
+            }
+
             String outputName = $"StructureDefinition_{this.sDef.Name}";
+            this.sDef.SaveJson(outputName);
         }
 
         public SDefEditor Name(String value)
