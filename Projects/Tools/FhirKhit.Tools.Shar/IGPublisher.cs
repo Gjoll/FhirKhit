9b6aaa65c87be5798a8a6bfc94da36955c1c599d
ROBIN\Kurt <KurtA@Gjoll.com>
2020-01-30 13:41:32 -0600

    .

---
+++ Projects/Tools/FhirKhit.Tools.Shar/IGPublisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+#if FHIR_R4
+namespace FhirKhit.Tools.R4
+#elif FHIR_R3
+namespace FhirKhit.Tools.R3
+#endif
+{
+    public class IGPublisher : ConverterProcess
+    {
+        public bool Publish(String executingDir, String jarPath, String igPath)
+        {
+            executingDir = Path.GetDirectoryName(executingDir);
+            executingDir = Path.GetFullPath(executingDir);
+            StringBuilder args = new StringBuilder();
+            args.Append($"-jar  \"{jarPath}\" ");
+            args.Append($"-ig \"{igPath}\"");
+            base.Execute(executingDir, "java", args.ToString());
+            return this.HasErrors;
+        }
+    }
+}
